78629b484e6c52ac63bf34932693b66ce8469a86
iliabulavintsev <twintipper@mail.ru>
2017-03-22 14:25:26 +0300

    add sort weight*age

--- app/src/main/java/com/example/ilia/json/User.java
+++ app/src/main/java/com/example/ilia/json/User.java
@@ -1,33 +1,19 @@
 package com.example.ilia.json;
 
 
+import android.util.Log;
 
-public class User implements Comparable<User>{
+public class User{
     private String fullName;
-    private int age;
-    private float weight;
+    public int age;
+    public float weight;
     private String gender;
     private String married;
     private boolean protecte;
-    public int check;
 
     public String toString() {
-        sort();
         StringBuilder all = new StringBuilder();
-        all.append("fullName: '" + this.fullName + "', age: '" + this.age + "', weight: '" + this.weight + "'\n");
+        all.append("fullName: '" + this.fullName + "', age: '" + this.age + "', weight: '" + this.weight +"kaff: "+ this.age*Math.abs(this.weight-50)+ "'\n");
         return all.toString();
-    }
-
-    public void sort() {
-        check = age * Math.abs((int)weight - 50);
-    }
-    public int getC() {
-        return check;
-    }
-
-    public int compareTo(User o) {
-        if (this.check > o.check){
-            return 1;
-        } else return  0;
     }
 }
--- app/src/main/java/com/example/ilia/json/jsonStruct.java
+++ app/src/main/java/com/example/ilia/json/jsonStruct.java
@@ -1,31 +1,45 @@
 package com.example.ilia.json;
 
 
+import android.util.Log;
+
+import com.google.common.base.Function;
+import com.google.common.collect.Ordering;
+import com.google.common.primitives.Floats;
+import com.google.common.primitives.Ints;
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
+
+import static junit.framework.Assert.assertEquals;
 
 public class jsonStruct {
 
     private int id;
     private String name;
     private int size;
-    private User[] users;
+    private List<User> users;
+
 
     public String toString() {
         StringBuilder data = new StringBuilder();
-        data.append("id: '" + this.id + "', name: '" + this.name + "', size: '" + this.size);
+        data.append("id: '" + this.id + "', name: '" + this.name + "', size: '" + this.size +"\n");
 
-        // 2. sort using comparator: sort by id
-        Arrays.sort(users, new Comparator<User>() {
-            @Override
-            public int compare(User o1, User o2) {
-                return o1.compareTo(o2);
-//                if (o1.check > o2.check){
-//                    return 1;
-//                } else return  0;
+        Comparator<User> byKaff = new Comparator<User>() {
+            public int compare(User left, User right) {
+                return Ints.compare(left.age*Math.abs((int)left.weight-50), right.age*Math.abs((int)right.weight-50));
             }
-        });
-        Arrays.sort(users);
+        };
+        Comparator<User> byK = Ordering.from(byKaff);
+
+        Collections.sort(users, byK);
+
+        Log.d("qq", users.toString());
+
         for (User user : users) {
             data.append("' users:" + user.toString());
         }
